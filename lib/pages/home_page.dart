8e1b8743e3ad681a69d5b1420e50dfdd4c65e261
KhoanDoanVan <doanvankhoan124@gmail.com>
2024-07-10 22:53:41 +0700

    tabbarui,foodmodel,config assets

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_fooddelivery/components/my_current_location.dart';
 import 'package:flutter_fooddelivery/components/my_description_box.dart';
 import 'package:flutter_fooddelivery/components/my_drawer.dart';
 import 'package:flutter_fooddelivery/components/my_silver_app_bar.dart';
+import 'package:flutter_fooddelivery/components/my_tab_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,7 +12,24 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _tabController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,18 +37,26 @@ class _HomePageState extends State<HomePage> {
       body: NestedScrollView(
         headerSliverBuilder: (context, innerBoxScrolled) => [
         MySilverAppBar(
+          title: MyTabBar(tabController: _tabController),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Divider(indent: 25, endIndent: 25,color: Theme.of(context).colorScheme.secondary,),
               const MyCurrentLocation(),
               const MyDescriptionBox()
             ],
           ),
-           title: Text('Title')
            )
       ], 
-      body: Container(color: Colors.blue,),),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          ListView.builder(itemBuilder: (context, index) => Text("first tab items")),
+          ListView.builder(itemBuilder: (context, index) => Text("second tab items")),
+          ListView.builder(itemBuilder: (context, index) => Text("third tab items"))
+      ])
+      ),
     );
   }
 }
